08dc6ae1390d39d82886e137e316e1721d2d9350
Leesa0 <palesa.k.mosebi@gmail.com>
2023-09-14 05:57:18 -0700

    0x14. C - Bit manipulation

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes a char to stdout
+ * @a: The character to print
+ *
+ * Return: success 1.
+ * error, -1 is returned
+ */
+int _putchar(char a)
+{
+	return (write(1, &a 1));
+}
